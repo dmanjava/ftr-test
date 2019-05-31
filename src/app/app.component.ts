048e7403cfdf424956704d3a5cd39ed24a2e59b1
Clarence Whitake <clarence_whitaker@contractor.nps.gov>
2019-05-31 15:29:50 -0600

    buttons and statuses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,31 @@
-import {Component, ViewChild} from '@angular/core';
-import {FrequencyDialogueComponent} from './components/frequency-dialogue/frequency-dialogue.component';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  @ViewChild('dialog') dialog: FrequencyDialogueComponent;
+export class AppComponent implements OnInit {
 
-  title = 'For The Record Test';
+  const APPSTATUS = AppStatus;
 
+  title = 'FTR Test App';
   frequency: number;
+
+  appStatus: string;
+
+  ngOnInit(): void {
+    this.setFrequency(0);
+    this.appStatus = AppStatus.INIT;
+  }
+
+  getAppStatus() {
+    return this.appStatus;
+  }
+
+  setAppStatus(s: string) {
+    this.appStatus = s;
+  }
 
   setFrequency(frequency: number) {
     this.frequency = frequency;
@@ -20,4 +34,23 @@ export class AppComponent {
   getFrequency(): number {
     return this.frequency;
   }
+
+  haltClick(e) {
+    this.setAppStatus(AppStatus.HALT);
+  }
+
+  resumeClick(e) {
+    this.setAppStatus(AppStatus.RESUME);
+  }
+
+  quitClick(e) {
+    this.setAppStatus(AppStatus.QUIT);
+  }
+}
+
+export enum AppStatus {
+  INIT = 'Initialized...',
+  HALT = 'Halted...',
+  RESUME = 'Resume...',
+  QUIT = 'Done.'
 }
